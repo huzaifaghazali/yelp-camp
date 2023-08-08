adfd0bbc85f351a68627b6c598e3b9ed1d52c22a
huzaifaghazali <huzaifaghazali@gmail.com>
2023-08-08 18:21:30 +0500

    Added description in validation

--- schemas.js
+++ schemas.js
@@ -7,5 +7,6 @@ module.exports.campgroundSchema = Joi.object({
      price: Joi.number().required().min(0),
      image: Joi.string().required(),
      location: Joi.string().required(),
+     description: Joi.string().required(),
    }).required(),
  });
