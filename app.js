8b560a9f3fd1581cfb2669c669f51945c82b38cc
huzaifaghazali <huzaifaghazali@gmail.com>
2023-08-09 19:22:49 +0500

    Added campgrounds router

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const Campground = require('./models/campgrounds');
 const Review = require('./models/review');
 const { campgroundSchema, reviewSchema } = require('./schemas');
 
+// Routes 
+const campgroundRoutes = require('./routes/campgrounds');
+
 // mongoDB
 const connectDB = require('./db/connect');
 
@@ -29,19 +32,6 @@ app.use(methodOverride('_method')); // Used to override the HTTP methods e.g URL
 
 const port = 3000;
 
-// Joi validation campground middleware
-const validateCampground = (req, res, next) => {
-  // validation the data as described in validation schema
-  const { error } = campgroundSchema.validate(req.body);
-
-  if (error) {
-    // Throw the error if data is invalid
-    const msg = error.details.map((el) => el.message).join(',');
-    throw new ExpressError(msg, 400);
-  } else {
-    next(); // Go to the next middleware function
-  }
-};
 
 // Joi validation review middleware
 const validateReview = (req, res, next) => {
@@ -61,86 +51,8 @@ app.get('/', (req, res) => {
   res.render('home'); // renders the 'home' view using the EJS templating
 });
 
-// Get all the campgrounds
-app.get('/campgrounds', async (req, res) => {
-  const campgrounds = await Campground.find({}); // Get all the Campgrounds
-  res.render('campgrounds/index', { campgrounds }); // renders the 'campground/index' ejs view and pass campgrounds to it.
-});
-
-// This route must be above the id route
-app.get('/campgrounds/new', async (req, res) => {
-  res.render('campgrounds/new'); // renders the 'campground/new' form ejs view
-});
-
-// Create Campground
-app.post(
-  '/campgrounds',
-  validateCampground,
-  catchAsync(async (req, res, next) => {
-    // Throw error if data doesn't exist then catchAsync will catch it and pass it to middleware.
-    if (!req.body.campground)
-      throw new ExpressError('Invalid Campground data', 400);
-
-    const data = req.body.campground;
-
-    // Create a new campground
-    const campground = new Campground(data);
-    await campground.save();
-
-    res.redirect(`/campgrounds/${campground._id}`); // Go to the newly created campground
-  })
-);
-
-// Get the specific Campground
-app.get(
-  '/campgrounds/:id',
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id).populate('reviews'); // Get the specific Campground and add reviews that are associated with it.
-    res.render('campgrounds/show', { campground }); // renders the 'campground/show' ejs view and pass campground to it.
-  })
-);
-
-// Get the specific Campground for edit
-app.get(
-  '/campgrounds/:id/edit',
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id); // Get the specific Campground
-    res.render('campgrounds/edit', { campground }); // renders the 'campground/edit' ejs view and pass campground to it.
-  })
-);
-
-// Update campground
-app.put(
-  '/campgrounds/:id',
-  validateCampground,
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const data = req.body.campground;
-
-    // Update the campground with specific ID
-    const campground = await Campground.findOneAndUpdate(
-      { _id: id }, // Filter criteria
-      { ...data } // Updated data
-    );
-
-    res.redirect(`/campgrounds/${campground._id}`); // Go to the newly updated campground
-  })
-);
-
-// Delete campground
-app.delete(
-  '/campgrounds/:id',
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-
-    // Delete the campground with specific ID
-    await Campground.findByIdAndDelete(id); // This will trigger the mongoose findOneAndDelete middleware
-
-    res.redirect(`/campgrounds`); // Go to the campgrounds
-  })
-);
+// Router middleware
+app.use('/campgrounds', campgroundRoutes);
 
 // Create Review
 app.post(
---
+++ routes/campgrounds.js
@@ -0,0 +1,111 @@
+// Packages
+const express = require('express');
+const router = express.Router();
+
+// Model
+const Campground = require('../models/campgrounds');
+const { campgroundSchema } = require('../schemas');
+
+// Errors
+const ExpressError = require('../utils/ExpressError');
+const catchAsync = require('../utils/catchAsync');
+
+// Joi validation campground middleware
+const validateCampground = (req, res, next) => {
+   // validation the data as described in validation schema
+   const { error } = campgroundSchema.validate(req.body);
+ 
+   if (error) {
+     // Throw the error if data is invalid
+     const msg = error.details.map((el) => el.message).join(',');
+     throw new ExpressError(msg, 400);
+   } else {
+     next(); // Go to the next middleware function
+   }
+ };
+
+ 
+
+// Get all the campgrounds
+router.get('/', async (req, res) => {
+  const campgrounds = await Campground.find({}); // Get all the Campgrounds
+  res.render('campgrounds/index', { campgrounds }); // renders the 'campground/index' ejs view and pass campgrounds to it.
+});
+
+// This route must be above the id route
+router.get('/new', async (req, res) => {
+  res.render('campgrounds/new'); // renders the 'campground/new' form ejs view
+});
+
+// Create Campground
+router.post(
+  '/',
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    // Throw error if data doesn't exist then catchAsync will catch it and pass it to middleware.
+    if (!req.body.campground)
+      throw new ExpressError('Invalid Campground data', 400);
+
+    const data = req.body.campground;
+
+    // Create a new campground
+    const campground = new Campground(data);
+    await campground.save();
+
+    res.redirect(`/campgrounds/${campground._id}`); // Go to the newly created campground
+  })
+);
+
+// Get the specific Campground
+router.get(
+  '/:id',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id).populate('reviews'); // Get the specific Campground and add reviews that are associated with it.
+    res.render('campgrounds/show', { campground }); // renders the 'campground/show' ejs view and pass campground to it.
+  })
+);
+
+// Get the specific Campground for edit
+router.get(
+  '/:id/edit',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id); // Get the specific Campground
+    res.render('campgrounds/edit', { campground }); // renders the 'campground/edit' ejs view and pass campground to it.
+  })
+);
+
+// Update campground
+router.put(
+  '/:id',
+  validateCampground,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const data = req.body.campground;
+
+    // Update the campground with specific ID
+    const campground = await Campground.findOneAndUpdate(
+      { _id: id }, // Filter criteria
+      { ...data } // Updated data
+    );
+
+    res.redirect(`/campgrounds/${campground._id}`); // Go to the newly updated campground
+  })
+);
+
+// Delete campground
+router.delete(
+  '/:id',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+
+    // Delete the campground with specific ID
+    await Campground.findByIdAndDelete(id); // This will trigger the mongoose findOneAndDelete middleware
+
+    res.redirect(`/campgrounds`); // Go to the campgrounds
+  })
+);
+
+
+module.exports = router;
