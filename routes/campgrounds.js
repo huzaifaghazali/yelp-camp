e4eb6553501e7794680a27f5f425cbbdea8cdf51
huzaifaghazali <huzaifaghazali@gmail.com>
2023-08-15 20:28:12 +0500

    Added multer middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,6 +1,8 @@
 // Packages
 const express = require('express');
 const router = express.Router();
+const multer = require('multer'); // handling multipart/form-data,
+const upload = multer({ dest: 'uploads/' }); // destination in which files are uploaded
 
 // Controllers
 const {
@@ -17,10 +19,16 @@ const {
 const { validateCampground } = require('../middleware/campground');
 const { isLoggedIn, isAuthor } = require('../middleware/authenticate');
 
+// Multer adds a body object and a file or files object to the request object.
+
 router
   .route('/')
   .get(showCampgrounds) // Get all the campgrounds
-  .post(isLoggedIn, validateCampground, createCampground); // Authenticate the user Check validations then Create Campground
+  // .post(isLoggedIn, validateCampground, createCampground) // Authenticate the user Check validations then Create Campground
+  .post(upload.array('image'), (req, res) => {
+    console.log(req.body, req.files);
+    res.send('it worked')
+  })
 
 // This route must be above the id route
 // Authenticate the user then show form
