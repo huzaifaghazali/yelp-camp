3e12cbb8284298f8442446ea4944a80b85b00a9e
huzaifaghazali <huzaifaghazali@gmail.com>
2023-08-21 18:52:42 +0500

    Updated code

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -12,10 +12,9 @@ map.on('load', () => {
   // Add a new source from our GeoJSON data and
   // set the 'cluster' option to true. GL-JS will
   // add the point_count property to your source data.
-  map.addSource('earthquakes', {
+  map.addSource('campgrounds', {
     type: 'geojson',
-    // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-    // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+    // Point to GeoJSON data. This example visualizes all M1.0+ campgrounds
     data: campgrounds,
     cluster: true,
     clusterMaxZoom: 14, // Max zoom to cluster points on
@@ -25,7 +24,7 @@ map.on('load', () => {
   map.addLayer({
     id: 'clusters',
     type: 'circle',
-    source: 'earthquakes',
+    source: 'campgrounds',
     filter: ['has', 'point_count'],
     paint: {
       // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
@@ -49,7 +48,7 @@ map.on('load', () => {
   map.addLayer({
     id: 'cluster-count',
     type: 'symbol',
-    source: 'earthquakes',
+    source: 'campgrounds',
     filter: ['has', 'point_count'],
     layout: {
       'text-field': ['get', 'point_count_abbreviated'],
@@ -61,11 +60,11 @@ map.on('load', () => {
   map.addLayer({
     id: 'unclustered-point',
     type: 'circle',
-    source: 'earthquakes',
+    source: 'campgrounds',
     filter: ['!', ['has', 'point_count']],
     paint: {
       'circle-color': '#11b4da',
-      'circle-radius': 4,
+      'circle-radius': 5,
       'circle-stroke-width': 1,
       'circle-stroke-color': '#fff',
     },
@@ -78,7 +77,7 @@ map.on('load', () => {
     });
     const clusterId = features[0].properties.cluster_id;
     map
-      .getSource('earthquakes')
+      .getSource('campgrounds')
       .getClusterExpansionZoom(clusterId, (err, zoom) => {
         if (err) return;
 
